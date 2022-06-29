e45353290021f241f2592509858d7d8bd6403961
Ofido <danieldsb98@gmail.com>
2022-06-28 22:50:09 -0300

    ok

--- describe_all.py
+++ describe_all.py
@@ -1,13 +1,52 @@
+import logging
 from os import listdir
-from os.path import isfile
 
-# isso fecha o arquivo sozinhos?
-projects = [line.strip() for line in open('.config', 'r')]
-arr = listdir()
+from genericpath import isdir
 
-for item in arr:
-    if not isfile(item):
-        if item not in projects:
-            list_item = listdir(item)
-            if ".git" in list_item:
-                projects.append(item)
+logging.getLogger().setLevel(logging.WARN)
+
+def verify_item_in_project(item, local=None):
+    if item not in projects:
+        logging.info("not in projects")
+        if local:
+            here = f"{local}\\{item}"
+        else:
+            here = f"{item}"
+        list_dir(here, item)
+
+def verify_itens_in_dir(dir, local):
+    for item in dir:
+        logging.info(f"item: {item}")
+        if isdir(item):
+            logging.info("not file")
+            if local:
+                here = f"{local}\\{item}"
+            else:
+                here = f"{item}"
+            list_dir(local=here)
+            verify_item_in_project(item, local)
+            continue
+
+def list_dir(local = None, item = None):
+    if not str(local).split("\\")[-1].startswith("."):
+        logging.info(f"local: {local}, item: {item}")
+        dir = listdir(local)
+        logging.info(f"dir: {dir}")
+        if item and (".git" in dir):
+            logging.info("have git")
+            projects.append(item)
+            return
+        verify_itens_in_dir(dir, local)
+
+logging.info("segunda")
+try:
+    projects = [line.strip() for line in open('.config', 'r')]
+except:
+    projects = []
+
+list_dir()
+logging.warning(projects)
+
+with open('.config', 'w') as f:
+    for project in projects:
+        f.write(project + "\n")
